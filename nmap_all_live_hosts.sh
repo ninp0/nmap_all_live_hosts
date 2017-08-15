96da3c4eff2021dc02f350e07b55fd5ea9cf3d6b
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-15 15:51:35 -0600

    Tune initial-rtt-timeout & max-rtt-timeout (Round-trip-timeout) flags in an effort to dramatically speed up port scans (include UDP)

--- nmap_all_live_hosts.sh
+++ nmap_all_live_hosts.sh
@@ -23,14 +23,14 @@ nmap -sn $ip_range -oG host_discovery_results.txt
 cat host_discovery_results.txt | awk '{print $2}' | grep -v Nmap | while read ip; do
   echo "Full TCP Scan: ${ip}"
   echo "Full TCP Scan: ${ip}" >> latest_tcp_results.txt
-  nmap --initial-rtt-timeout 39ms --max-rtt-timeout 100ms -n -Pn -sS -T4 -p 0-65535 -A -oA "${ip}_tcp_report" $ip
+  nmap --initial-rtt-timeout 39ms --max-rtt-timeout 100ms -n -Pn -sS -T5 -p 0-65535 -A -oA "${ip}_tcp_report" $ip
   cat "${ip}_tcp_report.nmap" >> latest_tcp_results.txt
   echo -e "--------------------------------------------------------------------------------\n\n\n" >> latest_tcp_results.txt
   echo -e "--------------------------------------------------------------------------------\n\n\n"
 
   echo -e "Default UDP Scan: ${ip}"
   echo -e "Default UDP Scan: ${ip}" >> latest_udp_results.txt
-  nmap --initial-rtt-timeout 39ms --max-rtt-timeout 100ms -n -sU -T4 -A -oA "${ip}_udp_report" $ip
+  nmap --initial-rtt-timeout 39ms --max-rtt-timeout 100ms -n -sU -T5 -A -oA "${ip}_udp_report" $ip
   cat "${ip}_udp_report.nmap" >> latest_udp_results.txt
   echo -e "--------------------------------------------------------------------------------\n\n\n" >> latest_udp_results.txt
   echo -e "--------------------------------------------------------------------------------\n\n\n"
